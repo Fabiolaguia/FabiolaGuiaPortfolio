e6ad49a131039aee90be12e2f57469bc54751f59
fabiolaguia <fabiolaguiaf@gmail.com>
2021-02-10 00:03:01 -0400

    modificado

---
+++ main.js
@@ -0,0 +1,43 @@
+var json_data = {
+    "habilidades":[
+    { "nombre":"Javascript",
+    "porcentaje": "65"
+    },
+    {
+    "nombre":"CSS",
+    "porcentaje": "65"
+    },{
+    "nombre":"HTML",
+    "porcentaje": "75"
+    },
+    ,{
+    "nombre":"Python",
+    "porcentaje": "85"
+    }
+    ]
+    };
+    
+    function llenar_skill(){
+    var padre = document.getElementById("skills");
+    var elemento = '';
+    json_data["habilidades"].forEach((e)=>{
+    elemento = elemento + '<div class="GrupoSkills"> <strong class="largo" style="width:'+e["porcentaje"]+'%;">'+ e["nombre"]+" "+e["porcentaje"]+'%</strong></div>';
+    padre.innerHTML = elemento;
+    })
+    }
+
+    var slideIndex = 0;
+showSlides();
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 5000); 
+
+    
+}
